8913b96187ddfb16827f7af212804817eca0ea17
bhaduri <bhaduri.anirban@gmail.com>
2020-11-28 21:53:53 +0530

    Hedwig

--- src/main/java/org/sonorus/core/dto/SpeechServicePaths.java
+++ src/main/java/org/sonorus/core/dto/SpeechServicePaths.java
@@ -12,7 +12,7 @@ import org.patronus.fractal.constants.FractalServicePaths;
  * @author bhaduri
  */
 public class SpeechServicePaths extends FractalServicePaths{
-    public static final String DGRF_SPEECH_BASE_URI = "http://localhost:8080/DGRFSpeechCoreService/webresources";
+    public static final String DGRF_SPEECH_BASE_URI = "http://localhost:8080/SonoursCoreService/webresources";
     public static final String DGRF_SPEECH_BASE = "dgrfspeech";
     public static final String WAV_TO_CSV_AND_UPLOAD = "wavupload";
     public static final String DELETE_SPEECH_EMO = "deletespeechemo";
